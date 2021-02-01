2c33a4b2f341a7a7b6f0ae441a0b2c2f3972864a
jay_wu <jay_wu@gss.com.tw>
2021-02-01 20:49:40 +0800

    💄 add fb and repo link

--- src/public/js/for_static_env/pages/menu.js
+++ src/public/js/for_static_env/pages/menu.js
@@ -1,20 +1,49 @@
-const menuPageContent = `
-<div id="menuPage">
-  <h1>選單</h1>
+const menuPageContent = `<div id="menuPage">
+<h1>選單</h1>
 
-  <div class="w3-container" style="max-width: 500px;">
-    <ul class="w3-ul w3-large menu-list">
-      <li id="menuHistoryItem" class="w3-hover-light-gray w3-border">
-        <div class="w3-bar w3-display-container">
+<div class="w3-container" style="max-width: 500px;">
+  <ul class="w3-ul w3-large menu-list">
+    <!-- START Item -->
+    <li id="menuHistoryItem" class="w3-hover-light-gray w3-border">
+      <div class="w3-bar w3-display-container">
+        <span class="left-icon-container">
           <i class="fas fa-history"></i>
-          歷史事件
-          <span class="w3-display-right">
-            <i class="material-icons">
-              keyboard_arrow_right
-            </i>
-          </span>
-        </div>
-      </li>
-    </ul>
-  </div>
+        </span>
+        歷史事件
+        <span class="w3-display-right">
+          <i class="material-icons">
+            keyboard_arrow_right
+          </i>
+        </span>
+      </div>
+    </li>
+    <!-- END Item -->
+    <!-- START Item -->
+    <li id="menuFacebookItem" class="w3-hover-light-gray w3-border">
+      <div class="w3-bar w3-display-container">
+        <span class="left-icon-container">
+          <i class="fab fa-facebook-square"></i>
+        </span>
+        粉絲專頁
+        <span class="w3-display-right">
+          <i class="fas fa-external-link-alt"></i>
+        </span>
+      </div>
+    </li>
+    <!-- END Item -->
+    <!-- START Item -->
+    <li id="menuGithubRepoItem" class="w3-hover-light-gray w3-border">
+      <div class="w3-bar w3-display-container">
+        <span class="left-icon-container">
+          <i class="fab fa-github" style="color: #302F2F"></i>
+        </span>
+        Github
+        <span class="w3-display-right">
+          <i class="fas fa-external-link-alt"></i>
+        </span>
+      </div>
+    </li>
+    <!-- END Item -->
+  </ul>
+</div>
 </div>`;
--- src/public/js/pages/menu.js
+++ src/public/js/pages/menu.js
@@ -1,9 +1,22 @@
 let menuHistoryItem;
+let menuFacebookItem;
+let menuGithubRepoItem;
 
 initMenuPage = async() => {
   menuHistoryItem = document.getElementById("menuHistoryItem");
+  menuFacebookItem = document.getElementById("menuFacebookItem");
+  menuGithubRepoItem = document.getElementById("menuGithubRepoItem");
 
   menuHistoryItem.addEventListener("click", () => {
     pushNamed("/History");
   });
+
+  menuFacebookItem.addEventListener("click", () => {
+    window.open('https://www.facebook.com/WJProgramer', '_blank');
+  });
+
+  menuGithubRepoItem.addEventListener("click", () => {
+    window.open('https://github.com/wjprogramer/wjprogramer.github.io', '_blank');
+  });
+
 }
