7d5a07a83247721d6994fbca897270874f111598
jay_wu <jay_wu@gss.com.tw>
2021-02-06 07:20:58 +0800

    💬 add info of skills page

--- src/public/js/for_static_env/pages/monsterSkills.js
+++ src/public/js/for_static_env/pages/monsterSkills.js
@@ -2,6 +2,10 @@ const monsterSkillsPageContent = `<h1>技能統計</h1>
 
 <p>統計五星以上，有雙技能</p>
 
+<ul>
+  <li>左邊直欄為主要技能，上面橫列為次要技能</li>
+</ul>
+
 <table id="monsterSkillsTable" class="w3-table w3-table-all tableFixHead">
   <thead>
     <tr>
